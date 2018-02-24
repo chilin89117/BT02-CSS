eebc54ea8a8bc062f3c4782430dd4bc8fabdf888
Chi Lin <chilin89117@gmail.com>
2018-02-23 21:32:46 -0500

    Clean-up

--- BT02-05-CSSTimeline/includes/footer.php
+++ BT02-05-CSSTimeline/includes/footer.php
@@ -1,26 +1,10 @@
 				</div> <!-- /. timeline -->
 			</div> <!-- /. container -->
 		</section>
-
 		<footer>
 			<div class="container">
-				<p>Copyright &copy; 2016, CSS Responsive Timeline</p>
+				<p>Copyright &copy; 2016-2018 CSS Responsive Timeline</p>
 			</div>
 		</footer>
 	</body>
 </html>
-<!--
-Sarah Manning
-Elizabeth Childs
-Katja Obinger
-Alison Hendrix
-Cosima Niehaus
-Helena
-Jennifer Fitzimmons
-Tony
-
-Danielle Fournier
-Janika Zingler
-Anna Giordano
-
--->
